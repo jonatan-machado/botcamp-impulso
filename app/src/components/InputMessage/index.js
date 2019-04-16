2e4337fed4590e9b40de3aa643d5f7ae840eb3df
Jonatan Machado <jonatanmachado35@gmail.com>
2019-04-16 17:49:28 -0300

    inputs configurados

---
+++ app/src/components/InputMessage/index.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const InputMessage = styled.input.attrs({
+  type: "text",
+  placeholder: "Diz ai..."
+})`
+  box-sizing: border-box;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.33);
+  width: 100vw;
+  height: 50px;
+  background-color: #fbfbff;
+  box-shadow: 0 -2px 8px 0 rgba(0, 0, 0, 0.1);
+  position: absolute;
+  bottom: 0;
+  border: none;
+  padding: 14px 11px 14px 16px;
+`;
+export default InputMessage;
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -3,13 +3,17 @@ import React from "react";
 import HeaderWrapper from "../../components/wrapper/HeaderWrapper";
 import LogoBotcamp from "../../components/LogoBotcamp";
 import LogoutBtnBotcamp from "../../components/LogoutBtnBotcamp";
+import InputMessage from "../../components/InputMessage";
 
 const Chat = () => {
   return (
-    <HeaderWrapper>
-      <LogoBotcamp small />
-      <LogoutBtnBotcamp />
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <LogoBotcamp small />
+        <LogoutBtnBotcamp />
+      </HeaderWrapper>
+      <InputMessage />
+    </>
   );
 };
 
